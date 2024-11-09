f9ba5dd6f7f75b54e00ad0533a8e95c050a1d307
Laura Gomez <lgomezvi@dyn172-30-116-164.wireless.uwo.pri>
2024-11-09 12:57:13 -0500

    Install Tailwind CSS and initialize configuration

---
+++ frontend/women-techmakers-2024/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html',               // If you have HTML files in the src folder
+    './src/**/*.{js,jsx,ts,tsx}',    // Include JavaScript/JSX/TypeScript files in src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
